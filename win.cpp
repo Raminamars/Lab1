8d3b999cb0ceb33942d6a5ff66dce83b48d95981
Raminamars <arp.278278@gmail.com>
2024-03-21 15:29:34 +0700

    Доделала 2.8

---
+++ win.cpp
@@ -0,0 +1,35 @@
+#include <win.h>
+#include <QHBoxLayout>
+
+
+Win::Win(QWidget *parent):QWidget(parent)// реализуем конструктор
+{
+this->setWindowTitle("Счетчик");
+label1 = new QLabel("Cчет по 1",this);
+label2 = new QLabel("Cчет по 5",this);
+edit1 = new Counter("0",this);
+edit2 = new Counter("0",this);
+calcButton=new QPushButton("+1",this);
+exitButton=new QPushButton("Выход",this);
+QHBoxLayout *layout1 = new QHBoxLayout();
+layout1->addWidget(label1);
+layout1->addWidget(label2);
+QHBoxLayout *layout2 = new QHBoxLayout();
+layout2->addWidget(edit1);
+layout2->addWidget(edit2);
+QHBoxLayout *layout3 = new QHBoxLayout();
+layout3->addWidget(calcButton);
+layout3->addWidget(exitButton);
+QVBoxLayout *layout4 = new QVBoxLayout(this);
+layout4->addLayout(layout1);
+layout4->addLayout(layout2);
+layout4->addLayout(layout3);
+// связь сигнала нажатия кнопки и слота закрытия окна
+connect(calcButton,&QPushButton::clicked, edit1,&Counter::add_one);
+connect(edit1,&Counter::tick_signal,edit2,&Counter::add_one);
+connect(exitButton,&QPushButton::clicked,this,Win::close);
+}
+//connect(объект у которого сигнал, функция к которая присоединяется,
+  // объект у которого слот, функция к которому присоединяем);
+//connect(fileExplorerWindow, &FileExplorerWindow::dataChangeSignal,
+      //  chartWindow, &ChartWindow::switchData);
---
+++ win.h
@@ -0,0 +1,42 @@
+#ifndef win_h
+#define win_h
+#include <QtGui>
+#include <QWidget>
+#include <QString>
+#include <QLineEdit>
+#include <QTextCodec>
+#include <QPushButton>
+#include <QLabel>
+
+class Counter:public QLineEdit
+{
+    Q_OBJECT //можем использовать слоты и сигналы
+public:
+    Counter(const QString & contents, QWidget *parent=0):
+        QLineEdit(contents,parent){}
+signals:
+    void tick_signal();
+public slots:// обьявляем слот
+    void add_one() // функция с результатом не конструктор
+    {
+        QString str=text();
+        int r=str.toInt();
+        if (r!=0 && r%5 ==0) emit tick_signal();
+        r++;
+        str.setNum(r);
+        setText(str);
+    }
+};
+class Win: public QWidget
+{
+    Q_OBJECT
+protected:
+    QTextCodec *codec;
+    QLabel *label1,*label2;
+    Counter *edit1,*edit2;
+    QPushButton *calcButton;
+    QPushButton *exitButton;
+public:
+    Win(QWidget *parent = 0);// конструктор
+};
+#endif
